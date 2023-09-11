55e3e58ae19bd2a5f425157c96c02fb245c0d383
rlawndks4 <rlawndks0423@naver.com>
2023-09-11 14:59:58 +0900

    feat: update

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-
-  return next(err);  const err = new APIError('API not found', httpStatus.NOT_FOUND);
+  const err = new APIError('API not found', httpStatus.NOT_FOUND);
+  return next(err);
 });
 let server = undefined
 const HTTP_PORT = 8001;
